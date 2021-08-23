88d405fd57d307b346178706f9f60bc43d95d33d
godspeed7 <pawelpikus7@gmail.com>
2021-08-23 11:59:47 +0200

    handling completed items

--- src/App.js
+++ src/App.js
@@ -5,29 +5,65 @@ import TodoItem from './TodoItem'
 class App extends React.Component {
   constructor(){
     super()
-    this.state= {}
+    this.state = {
+      todos: []
+    }
+    this.handleChange = this.handleChange.bind(this)
   }
 
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users/1/todos')
     .then(res => res.json())
-    .then(data => {
-      this.setState({todos: data})
+    .then(
+      (result) => { this.setState(
+        {
+          isLoaded: true,
+          todos: result
+        });
+    },
+    (error) => { this.setState(
+      {
+        isLoaded: true,
+        error
+      });
+    }
+  )
+}
+
+  handleChange(id){
+    this.setState(prevState => {
+      const updatedItems = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+        return todo;
+      });
+      return { todos: updatedItems };
     })
   }
 
-  componentDidUpdate(){
-
-  }
-
   render() {
-    const todoItemsArr = this.state.todos && this.state.todos.map(todo => <TodoItem key={todo.id} title={todo.title}/>)
-    return ( 
-    <div className = "container" >
-      <h1 className="title">My Todos</h1>
-      {todoItemsArr}
-    </div>
-    )
+    const { error, isLoaded, todos } = this.state;
+
+    if(error){
+        return <div>Error: {error.message}</div>;
+    } else if(!isLoaded){
+        return <div>Loading...</div>;
+    } else {
+      
+      const todoItemsArr = todos && todos.slice(0,5)
+      .map(todo => <TodoItem key={todo.id} item = {todo} handleChange={this.handleChange}/>)
+      
+      return ( 
+        <div className = "container" >
+          <h1 className="title">My Todos</h1>
+          {todoItemsArr}
+        </div>
+      )
+    }
   }
 }
 
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,8 +3,11 @@ import './TodoItem.css'
 function TodoItem(props){
     return (
         <div className="todo">
-            <input type="checkbox" name="tick" id="checkbox-tick" defaultChecked="true"/>
-            <p className="todo-text">{props.title}</p>
+            <input 
+                type="checkbox" 
+                checked={props.item.completed}
+                onChange={() => props.handleChange(props.item.id)}/>
+            <p className="todo-text">{props.item.title}</p>
             
         </div>
     )
